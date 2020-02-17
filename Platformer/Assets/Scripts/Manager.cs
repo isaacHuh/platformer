0f8a8cb4df7f2eefae713aa688677dd81ba06505
Isaac Torres <torresisaac26@gmail.com>
2020-02-16 21:02:06 -0800

    project

---
+++ Platformer/Assets/Scripts/Manager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+    int score = 0;
+    int coins = 0;
+
+    float currentTime = 0f;
+    float startingTime = 300f;
+
+    public Text timeText;
+    public Text coinText;
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+
+        string timeTxt = "Time: " + (int)currentTime;
+        timeText.text = timeTxt;
+
+        string coinTxt = "Coin: " + coins;
+        coinText.text = coinTxt;
+
+        string scoreTxt = "Score: " + score;
+        scoreText.text = scoreTxt;
+    }
+
+    public void increaseCoins() {
+        coins++;
+    }
+}
---
+++ Platformer/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public float spd = 1f;
+    public GameObject manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( Input.GetMouseButtonDown (0)){ 
+            RaycastHit hit; 
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+            if ( Physics.Raycast (ray,out hit,100.0f)) {
+                GameObject box = hit.collider.gameObject;
+                if (box.tag == "breakable") {
+                    Debug.Log("break");
+                    Destroy(box);
+                }
+
+                if (box.tag == "coin") {
+                    manager.GetComponent<Manager>().increaseCoins();
+                }
+            }
+        }
+
+        bool keyRight = Input.GetKey(KeyCode.D);
+        bool keyLeft = Input.GetKey(KeyCode.A);
+
+        if (keyRight)
+        {
+            moveRight();
+        }
+
+        if (keyLeft)
+        {
+            moveLeft();
+        }
+    }
+
+    private void moveRight()
+    {
+        gameObject.transform.position += new Vector3(spd,0,0);
+    }
+
+    private void moveLeft()
+    {
+        gameObject.transform.position -= new Vector3(spd, 0, 0);
+    }
+}
